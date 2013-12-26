fd62fd824fb51abef0ede0341f124e570872aa54
Max Paperno <mpaperno@users.noreply.github.com>
2013-12-26 16:11:55 +0000

    quatos renaming for mot mixes.

--- ground/mot_mixes.bat
+++ ground/mot_mixes.bat
@@ -1,16 +1,16 @@
-l1Tool -c quad_plus -m -o mot_mixes.xml
-l1Tool -c quad_x -m -o mot_mixes.xml
-l1Tool -c hexa_plus -m -o mot_mixes.xml
-l1Tool -c hexa_x -m -o mot_mixes.xml
-l1Tool -c hexa_h -m -o mot_mixes.xml
-l1Tool -c hexa_coax -m -o mot_mixes.xml
-l1Tool -c hexa_coax_rev -m -o mot_mixes.xml
-l1Tool -c octo_plus -m -o mot_mixes.xml
-l1Tool -c octo_x -m -o mot_mixes.xml
-l1Tool -c octo_coax_plus -m -o mot_mixes.xml
-l1Tool -c octo_coax_x -m -o mot_mixes.xml
-l1Tool -c octo_colin_plus -m -o mot_mixes.xml
-l1Tool -c octo_colin_x -m -o mot_mixes.xml
-rem l1Tool -c octo_u -m -o mot_mixes.xml
-l1Tool -c octo_square_plus -m -o mot_mixes.xml
-l1Tool -c octo_square_x -m -o mot_mixes.xml
+quatosTool -c quad_plus -m -o mot_mixes.xml
+quatosTool -c quad_x -m -o mot_mixes.xml
+quatosTool -c hexa_plus -m -o mot_mixes.xml
+quatosTool -c hexa_x -m -o mot_mixes.xml
+quatosTool -c hexa_h -m -o mot_mixes.xml
+quatosTool -c hexa_coax -m -o mot_mixes.xml
+quatosTool -c hexa_coax_rev -m -o mot_mixes.xml
+quatosTool -c octo_plus -m -o mot_mixes.xml
+quatosTool -c octo_x -m -o mot_mixes.xml
+quatosTool -c octo_coax_plus -m -o mot_mixes.xml
+quatosTool -c octo_coax_x -m -o mot_mixes.xml
+quatosTool -c octo_colin_plus -m -o mot_mixes.xml
+quatosTool -c octo_colin_x -m -o mot_mixes.xml
+rem quatosTool -c octo_u -m -o mot_mixes.xml
+quatosTool -c octo_square_plus -m -o mot_mixes.xml
+quatosTool -c octo_square_x -m -o mot_mixes.xml
